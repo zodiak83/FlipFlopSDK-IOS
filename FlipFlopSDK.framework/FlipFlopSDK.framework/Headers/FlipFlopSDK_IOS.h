a7997266d7f053be1a93b5c0365f3949120ae7c7
Jocoos <57648837+jocoos-dev@users.noreply.github.com>
2021-08-06 15:13:02 +0900

    change library settings

---
+++ FlipFlopSDK.framework/FlipFlopSDK.framework/Headers/FlipFlopSDK_IOS.h
@@ -0,0 +1,18 @@
+//
+//  FlipFlopSDK_IOS.h
+//  FlipFlopSDK_IOS
+//
+//  Created by Jeon HyungGue on 2021/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlipFlopSDK_IOS.
+FOUNDATION_EXPORT double FlipFlopSDK_IOSVersionNumber;
+
+//! Project version string for FlipFlopSDK_IOS.
+FOUNDATION_EXPORT const unsigned char FlipFlopSDK_IOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlipFlopSDK_IOS/PublicHeader.h>
+
+
